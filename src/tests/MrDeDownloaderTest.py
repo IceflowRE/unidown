7124e233d1ffbc20ebaf8ac851cc81c2354ef561
Iceflower <iceflower@gmx.de>
2017-01-24 16:16:01 +0100

    [test_env] Add first test cases

---
+++ src/tests/MrDeDownloaderTest.py
@@ -0,0 +1,40 @@
+import unittest
+import MrDeDownloader as Mdd
+
+
+class TestMrDeDownloader(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        Mdd.init()
+
+    def test_MrDeDownloader_app_update_check(self):
+        """
+        |MrDeDownloader| check for app updates:
+        :return:
+        """
+        # check if github is up
+        try:
+            origin_version = Mdd.get_current_app_version()
+        except Exception:
+            self.skipTest('Connection to Github failed.')
+
+        # check every position of the version tag each
+        # version is older - update
+        for i in range(0, 3):
+            Mdd.VERSION = origin_version.copy()
+            Mdd.VERSION[i] = str(int(origin_version[i]) - 1)
+            with self.subTest(version=origin_version, test=Mdd.VERSION):
+                self.assertTrue(Mdd.check_for_app_updates())
+
+        # version is newer - no update
+        for i in range(0, 3):
+            Mdd.VERSION = origin_version.copy()
+            Mdd.VERSION[i] = str(int(origin_version[i]) + 1)
+            with self.subTest(version=origin_version, test=Mdd.VERSION):
+                self.assertFalse(Mdd.check_for_app_updates())
+
+        # version is equal - no update
+        Mdd.VERSION = origin_version.copy()
+        with self.subTest(version=origin_version, test=Mdd.VERSION):
+            self.assertFalse(Mdd.check_for_app_updates())
