14573862bf8e73db9ae3c045a6a831335b095137
Iceflower <iceflower@gmx.de>
2017-05-12 13:09:29 +0200

    [testing] Add some tests

--- tests/MrDeDownloaderTest.py
+++ tests/MrDeDownloaderTest.py
@@ -2,6 +2,8 @@
 Test for MrDeDownloader module.
 """
 import unittest
+from pathlib import Path
+
 import MrDeDownloader as Mdd
 
 
@@ -13,6 +15,38 @@ class TestMrDeDownloader(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         Mdd.init()
+
+    def test_delete_dr_rec(self):
+        """
+        |MrDeDownloader| delete directories recursive.
+        """
+        Path("./testtemp/").mkdir()
+        file = open("./testtemp/1", 'w')
+        file.close()
+        file = open("./testtemp/2", 'w')
+        file.close()
+        file = open("./testtemp/3", 'w')
+        file.close()
+        file = open("./testtemp/4", 'w')
+        file.close()
+        Path("./testtemp/temp/").mkdir()
+        Path("./testtemp/temp2/").mkdir()
+        file = open("./testtemp/temp/1", 'w')
+        file.close()
+        file = open("./testtemp/temp/2", 'w')
+        file.close()
+        file = open("./testtemp/temp/3", 'w')
+        file.close()
+        file = open("./testtemp/temp/4", 'w')
+        file.close()
+        Mdd.delete_dir_rec(Path("./testtemp/"))
+        self.assertFalse(Path("./testtemp/").exists())
+
+    def test_about(self):
+        """
+        |MrDeDownloader| about.
+        """
+        Mdd.about()
 
     def test_app_update_check(self):
         """
