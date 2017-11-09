979fd7f986cfbcb372bdf2160fa211a3115001e2
Iceflower <iceflower@gmx.de>
2017-11-09 13:04:13 +0100

    [build] Fix get app_version

--- scripts/new_release.sh
+++ scripts/new_release.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # executed from project root
 
-app_version=$(grep -oP "VERSION\s=\s'\K\w+.\w+.\w+" ./unidown/core/data/static.py)
+app_version=$(grep -oP "VERSION\s=\s'\K([\w\W]*)'" ./unidown/core/data/static.py)
+app_version=${app_version:: -1}
 
 # change online version for updater
 echo "$app_version" > ./version
--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -2,7 +2,8 @@
 # executed from project root
 
 py_version="$1"
-app_version=$(grep -oP "VERSION\s=\s'\K\w+.\w+.\w+" ./unidown/core/data/static.py)
+app_version=$(grep -oP "VERSION\s=\s'\K([\w\W]*)'" ./unidown/core/data/static.py)
+app_version=${app_version:: -1}
 
 ./scripts/build.sh "$py_version"
 pip install --upgrade --force-reinstall --no-cache ./dist/Universal_Downloader-"$app_version"-"$py_version"-none-any.whl[dev]
