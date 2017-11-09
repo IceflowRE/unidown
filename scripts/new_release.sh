b822bb0fe980b4602010e91333136407d6cf71fd
Iceflower <iceflower@gmx.de>
2017-11-09 11:19:22 +0100

    [build] Fix diversal things

--- scripts/new_release.sh
+++ scripts/new_release.sh
@@ -4,4 +4,4 @@
 app_version=$(grep -oP "VERSION\s=\s'\K\w+.\w+.\w+" ./unidown/core/data/static.py)
 
 # change online version for updater
-echo -e "$app_version""\c" > ./version
+echo "$app_version" > ./version
