4f52723c4dca8ae13e7579b79fc338d882e733d3
Iceflower <iceflower@gmx.de>
2018-05-25 14:34:54 +0200

    [refactor] Fix intendation

--- unidown/plugins/test/plugin.py
+++ unidown/plugins/test/plugin.py
@@ -17,11 +17,12 @@ class Plugin(APlugin):
             super().__init__(info)
 
     def _create_download_links(self):
-        return {'/IceflowRE/Universal-Downloader/master/README.rst':
-                    LinkItem('README.rst', datetime(2000, 1, 1, hour=1, minute=1, second=1)),
-                '/IceflowRE/Universal-Downloader/master/no_file_here':
-                    LinkItem('LICENSE', datetime(2002, 2, 2, hour=2, minute=2, second=2))
-                }
+        return {
+            '/IceflowRE/Universal-Downloader/master/README.rst':
+                LinkItem('README.rst', datetime(2000, 1, 1, hour=1, minute=1, second=1)),
+            '/IceflowRE/Universal-Downloader/master/no_file_here':
+                LinkItem('LICENSE', datetime(2002, 2, 2, hour=2, minute=2, second=2))
+        }
 
     def _create_last_update_time(self):
         return datetime(1999, 9, 9, hour=9, minute=9, second=9)
