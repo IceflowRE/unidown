37c97e9c732cbdb759453849ec38ebac1f3c4fe6
Iceflower <iceflower@gmx.de>
2018-09-06 15:32:52 +0200

    [testing] Fix main test

--- tests/main_test.py
+++ tests/main_test.py
@@ -22,13 +22,13 @@ class DynamicDataTest(unittest.TestCase):
         delete_dir_rec(Path('./tmp/'))
         with self.subTest(desc="no delay"):
             with self.assertRaises(SystemExit) as se:
-                main(['/usr/bin/unidown', '--main', './tmp/', '--plugin', 'test', '--log', 'CRITICAL'])
+                main(['--main', './tmp/', '--plugin', 'test', '--log', 'CRITICAL'])
 
             self.assertEqual(se.exception.code, 0)
             self.assertTrue(Path('./tmp/savestates/test_save.json').exists())
             self.assertTrue(Path('./tmp/downloads/test/README.rst').exists())
         with self.subTest(desc="print list"):
             with self.assertRaises(SystemExit) as se:
-                main(['/usr/bin/unidown', '--plugin-list'])
+                main(['--list-plugins'])
 
             self.assertEqual(se.exception.code, 0)
