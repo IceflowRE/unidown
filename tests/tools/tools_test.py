e9f990819460460d54ffa18c78a4782e6e8c5150
Iceflower <iceflower@gmx.de>
2019-02-01 11:28:22 +0100

    [testing] Attempt to fix tests

--- tests/tools/tools_test.py
+++ tests/tools/tools_test.py
@@ -6,18 +6,18 @@ from unidown.tools import delete_dir_rec
 
 class ToolsTest(unittest.TestCase):
     def test_delete_dir_rec(self):
-        Path("./testtemp/").mkdir()
+        Path("./test_delete_dir_rec/").mkdir()
         for number in range(1, 4):
-            file = open("./testtemp/" + str(number), 'w')
+            file = open("./test_delete_dir_rec/" + str(number), 'w')
             file.close()
-        Path("./testtemp/temp/").mkdir()
+        Path("./test_delete_dir_rec/temp/").mkdir()
         for number in range(1, 4):
-            file = open("./testtemp/temp/" + str(number), 'w')
+            file = open("./test_delete_dir_rec/temp/" + str(number), 'w')
             file.close()
-        Path("./testtemp/temp2/").mkdir()
-        delete_dir_rec(Path("./testtemp/"))
+        Path("./test_delete_dir_rec/temp2/").mkdir()
+        delete_dir_rec(Path("./test_delete_dir_rec/"))
 
-        self.assertFalse(Path("./testtemp/").exists())
+        self.assertFalse(Path("./test_delete_dir_rec/").exists())
 
         self.assertFalse(Path("./donotexist/").exists())
         delete_dir_rec(Path("./donotexist/"))
