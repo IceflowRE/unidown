f3d3ee7a107215ba471c4c24d9ca0dc793341b83
Iceflower <iceflower@gmx.de>
2019-02-01 11:27:19 +0100

    [dev] Remove script

--- scripts/new_release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# executed from project root
-
-app_version=$(grep -oP "VERSION\s=\s'\K([\w\W]*)'" ./unidown/static_data.py)
-app_version=${app_version:: -1}
-
-# change online version for updater
-echo "$app_version" > ./version
