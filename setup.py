64512cd0828b9dac34d0ee10fb22f8b95fed61bf
Iceflower <iceflower@gmx.de>
2020-03-29 00:18:53 +0100

    [general] Python 3.8 or higher is required

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     ],
     keywords='modular downloader',
     packages=find_packages(include=['unidown', 'unidown.*']),
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         'urllib3[secure]==1.25.8',
         'tqdm==4.35.0',
