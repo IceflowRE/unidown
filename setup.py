49f0154db148d9176b74c77abec735d72049e9ab
Iceflower <iceflower@gmx.de>
2020-04-03 21:31:23 +0200

    [pypi] Fix development status

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
